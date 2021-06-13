97e4f4d83ee7d2539b2067cc11879d09f528359b
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2021-06-13 14:16:23 +0300

    Added a doc block

--- core/database/Connection.php
+++ core/database/Connection.php
@@ -1,16 +1,26 @@
 <?php
-  namespace App\Core\Database; 
-class Connection {
+namespace App\Core\Database; 
 
+class Connection {
+    /**
+     * Make Database connection.
+     *
+     * @param config[] configuration data from config.php
+     * 
+     * @return \PDO New PDO instance
+     */
     public static function make($config) {
 
         try {
-            /* fall back code ie the config is absent
-            return new PDO("mysql:host=localhost;dbname=myapp", 'root', ''); */
+            /**
+             *fall back code ie the config is absent
+            */ 
+
+            // return new PDO("mysql:host=localhost;dbname=myapp", 'root', ''); 
 
             //Here you return the credentials stored in the config file
             return new \PDO(
-                $config['connection'] . ';dbname=' . $config['name'],
+                $config['connection']. $config['port'] . ';dbname=' . $config['name'],
                 $config['username'],
                 $config['password'],
                 $config['options']
