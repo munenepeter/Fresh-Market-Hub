38ca8ff9a2869eb8b408073fe33073c4194c4950
Peter Munene <peternjega7@gmail.com>
2021-07-11 15:51:52 +0300

    Redesign of empty cart

--- views/cart.view.php
+++ views/cart.view.php
@@ -144,7 +144,16 @@ if (isset($_POST['remove'])) {
     </div>
 
   <?php } else {
-    echo 'Cart is Empty....!';
+    echo '
+    <section class="flex items-center mx-10 h-full sm:p-16 bg-gray-50 text-gray-800">
+    <div class="container flex flex-col items-center justify-center px-5 mx-auto my-8 space-y-8 text-center sm:max-w-md">  
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-40 h-40 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+      <p class="text-3xl">Looks like your cart is empty!</p>
+      <a href="#" class="px-8 py-3 font-semibold rounded bg-blue-600 text-gray-50">Back to products</a>
+    </div>
+  </section>';
   } ?>
 
 </div>
